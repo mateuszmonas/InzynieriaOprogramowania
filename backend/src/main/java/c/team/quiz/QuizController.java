f5354d8be7b9ecedeab6ca7bca6bf0c03cc20bbb
mm3 <mateusz.monastyrski@softnet.com.pl>
2021-04-13 17:17:01 +0200

    feature/#44/creating_quizzes

--- backend/src/main/java/c/team/quiz/QuizController.java
+++ backend/src/main/java/c/team/quiz/QuizController.java
@@ -6,15 +6,16 @@ import c.team.quiz.model.CreateQuizRequest;
 import c.team.quiz.model.CreateQuizResponse;
 import c.team.quiz.model.GetQuizzesResponse;
 import c.team.quiz.model.Quiz;
+import c.team.security.model.UserPrincipal;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -25,23 +26,22 @@ public class QuizController {
     private final QuizService quizService;
 
     @PostMapping
-    public ResponseEntity<CreateQuizResponse> createQuiz(@AuthenticationPrincipal User user, @RequestBody @Valid CreateQuizRequest request) {
-        String userId = userAccountService.findByUsername(user.getUsername()).getId();
-        CreateQuizResponse response = new CreateQuizResponse(quizService.createQuiz(userId, request));
+    public ResponseEntity<CreateQuizResponse> createQuiz(@AuthenticationPrincipal UserPrincipal user, @RequestBody @Valid CreateQuizRequest request) {
+        String quizId = quizService.createQuiz(user.getId(), request);
+        CreateQuizResponse response = new CreateQuizResponse(quizId);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
-    public ResponseEntity<GetQuizzesResponse> getQuizzes(@AuthenticationPrincipal User user) {
-        String userId = userAccountService.findByUsername(user.getUsername()).getId();
-        GetQuizzesResponse response = new GetQuizzesResponse(quizService.getUserQuizzes(userId));
+    public ResponseEntity<GetQuizzesResponse> getQuizzes(@AuthenticationPrincipal UserPrincipal user) {
+        List<Quiz> quizzes = quizService.getUserQuizzes(user.getId());
+        GetQuizzesResponse response = new GetQuizzesResponse(quizzes);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("{quizId}")
-    public ResponseEntity<Quiz> getQuiz(@AuthenticationPrincipal User user, @PathVariable String quizId) {
-        String userId = userAccountService.findByUsername(user.getUsername()).getId();
-        Quiz quiz = quizService.findQuiz(userId, quizId).orElseThrow();
+    public ResponseEntity<Quiz> getQuiz(@AuthenticationPrincipal UserPrincipal user, @PathVariable String quizId) {
+        Quiz quiz = quizService.findQuiz(user.getId(), quizId).orElseThrow();
         return ResponseEntity.ok(quiz);
     }
 
