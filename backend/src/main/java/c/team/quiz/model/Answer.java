90418e6999e68bf489977d1f176b59d51f2e00ed
mm3 <mateusz.monastyrski@softnet.com.pl>
2021-04-18 15:44:29 +0200

    added open ended questions

---
+++ backend/src/main/java/c/team/quiz/model/Answer.java
@@ -0,0 +1,14 @@
+package c.team.quiz.model;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Answer {
+    @Parameter(required = true)
+    private String text;
+    @Parameter(required = true)
+    private boolean isCorrect;
+}
