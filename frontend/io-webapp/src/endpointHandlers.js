469aba3dcdd8d323602920c99bbf4304bc6f7c3e
Observer46 <305391@student.agh.edu.pl>
2021-06-07 23:46:00 +0200

    Fixed creating quizzes during session

--- frontend/io-webapp/src/endpointHandlers.js
+++ frontend/io-webapp/src/endpointHandlers.js
@@ -1,32 +1,62 @@
 export const getParticipantsHandler = (e, state, dispatch) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    (async () => {
-      await fetch(process.env.REACT_APP_BACKEND_URL + `/session/${state.sessionId}/participant/list`, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              identification: state.stage === "lecturer" ? state.username : state.guestId,
-          }),
-      })
-          .then((response) => response.json())
-          .then((data) => {
-          dispatch({
-            type: "SET_SESSION_OWNER",
-            payload: data.leaderAccountName,
-          });
-          dispatch({
-            type: "SET_PARTICIPANTS",
-            payload: [
-              { id: "", username: data.leaderAccountName, approved: true },
-              ...data.participants,
-            ],
-          });
-        })
-        .catch((error) => {
-          console.error(error);
+  (async () => {
+    await fetch(process.env.REACT_APP_BACKEND_URL + `/session/${state.sessionId}/participant/list`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            identification: state.stage === "lecturer" ? state.username : state.guestId,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+        dispatch({
+          type: "SET_SESSION_OWNER",
+          payload: data.leaderAccountName,
         });
-    })();
-  };
+        dispatch({
+          type: "SET_PARTICIPANTS",
+          payload: [
+            { id: "", username: data.leaderAccountName, approved: true },
+            ...data.participants,
+          ],
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })();
+};
+
+export const createHandler = (e, state, dispatch) => {
+  e.preventDefault();
+  console.log(state);
+  (async () => {
+    await fetch(process.env.REACT_APP_BACKEND_URL + "/quiz", {
+      method: "POST",
+      headers: {
+        Authorization: state.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: state.quizName,
+        questions: state.designerQuestions,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (state.stage === "designer")
+          dispatch({ type: "SET_STAGE_QUIZ_LIST" });
+        else
+          state.quizId = data.quizId;
+        console.log(state);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })();
+};
