a9abef76b6c9e5832c922f14c64d4bac75940afa
Konrad Bogusz <56386894+kdbogusz@users.noreply.github.com>
2021-06-08 20:27:30 +0200

    [#16][#18][#24][#108][#128] Finalna wersja statystyk (#145)
    * finally working graphs
    
    * added answer cloud
    
    * removed email field from sign up form

--- frontend/io-webapp/src/session/Stats.js
+++ frontend/io-webapp/src/session/Stats.js
@@ -1,126 +1,179 @@
 import React from "react";
-import {Bar, BarChart, CartesianGrid, Cell, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis,} from "recharts";
-import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { TagCloud } from "react-tagcloud";
 
 import "./stats.css";
 
-const Stats = ({state, dispatch}) => {
+const Stats = ({ state, dispatch }) => {
   const [picked, setPicked] = React.useState(0);
   const [questions, setQuestions] = React.useState([]);
 
   const getStats = () => {
     (async () => {
-      await fetch(process.env.REACT_APP_BACKEND_URL + `/session/${state.sessionId}/statistics/answers`, {
-        method: "GET",
-        headers: {
-          Authorization: state.token,
-          "Content-Type": "application/json",
-        },
-      })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            const dataReplies = [];
+      await fetch(
+        process.env.REACT_APP_BACKEND_URL +
+          `/session/${state.sessionId}/statistics/answers`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: state.token,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setQuestions(
+            data.sessionAnswers.map((q) => {
+              const dataReplies = [];
+              for (let i = 0; i < q.question.answers.length; i++) {
+                dataReplies.push({
+                  answers: q.answerCounts[i],
+                  name: q.question.answers[i].text,
+                  isCorrect: q.question.answers[i].correct,
+                });
+              }
 
-          for (const property in data.sessionAnswers.answerCounts) {
-            const idx = parseInt(property.substring(14));
-            dataReplies.push({
-              answers: data.sessionAnswers.answerCounts[property],
-              name: data.sessionAnswers.question.answers[idx].text,
-              isCorrect: data.sessionAnswers.question.answers[idx].correct
-            });
-          }
-
-          setQuestions(data.sessionAnswers.map((q) => {return ({
-            content: q.question.content,
-            answers: q.question.answers.map((answer) => {return answer.text}),
-            replies: dataReplies
-          })}));
+              return {
+                content: q.question.content,
+                answers: q.question.answers.map((answer) => {
+                  return answer.text;
+                }),
+                replies: dataReplies,
+                open: q.question.open,
+              };
+            })
+          );
         })
         .catch((error) => {
           console.error(error);
         });
     })();
-  }
+  };
 
   React.useEffect(() => {
     const interval = setInterval(() => getStats(), 5000);
     return () => {
       clearInterval(interval);
     };
-  }, [])
-
-  // const sampleHistogram = [
-  //   { students: 0, name: "0-10%" },
-  //   { students: 1, name: "10-20%" },
-  //   { students: 3, name: "20-30%" },
-  //   { students: 5, name: "30-40%" },
-  //   { students: 10, name: "40-50%" },
-  //   { students: 11, name: "50-60%" },
-  //   { students: 7, name: "60-70%" },
-  //   { students: 6, name: "70-80%" },
-  //   { students: 4, name: "80-90%" },
-  //   { students: 3, name: "90-100%" },
-  // ];
-
-  // const sampleQuestions = [
-  //   {
-  //     id: 1,
-  //     question: "What's the first letter of the alphabet?",
-  //     answers: ["A", "D", "F", "P"],
-  //     replies: [
-  //       { answers: 3, name: "A", isCorrect: false },
-  //       { answers: 8, name: "B", isCorrect: true },
-  //       { answers: 2, name: "C", isCorrect: false },
-  //       { answers: 2, name: "D", isCorrect: false },
-  //     ],
-  //   },
-  //   {
-  //     id: 2,
-  //     question: "What colour is the water in Cracow?",
-  //     answers: ["Green", "Blue", "Black", "Brown"],
-  //     replies: [
-  //       { answers: 7, name: "A", isCorrect: true },
-  //       { answers: 6, name: "B", isCorrect: false },
-  //       { answers: 3, name: "C", isCorrect: false },
-  //       { answers: 12, name: "D", isCorrect: true },
-  //     ],
-  //   },
-  // ];
+  }, []);
 
   return (
-    <div className="statsView" >
+    <div className="statsView">
+      <div className="statsGraphPicker">
+        <FiChevronLeft
+          size={32}
+          onClick={() =>
+            setPicked((picked + questions.length) % questions.length)
+          }
+        />
+        {questions.map((question, index) => {
+          return (
+            <div
+              onClick={() => setPicked(index)}
+              style={picked === index ? {} : { display: "none" }}
+            >
+              {question.content}
+            </div>
+          );
+        })}
+        <FiChevronRight
+          size={32}
+          onClick={() => setPicked((picked + 1) % questions.length)}
+        />
+      </div>
 
-    <div className="statsGraphPicker">
-      <FiChevronLeft size={32} onClick={() => setPicked((picked + questions.length) % (questions.length + 1))} />
-      {questions.map((question, index) => {
-        return (
-          <div
-            onClick={() => setPicked(index)}
-            style={picked === index ? {} : {display: "none"}}
-          >
-            {question.content}
-          </div>
-        );
-      })}
-      <FiChevronRight size={32} onClick={() => setPicked((picked + 1) % (questions.length + 1))} />
-    </div>
+      {questions.length === 0 ? (
+        <></>
+      ) : questions[picked].open ? (
+        <TagCloud
+          style={{
+            height: "100%",
+            width: "95%",
+            alignSelf: "center",
+          }}
+          minSize={12}
+          maxSize={47}
+          colorOptions={{
+            luminosity: "dark",
+            hue: "blue",
+          }}
+          shuffle={false}
+          disableRandomColor={true}
+          tags={questions[picked].replies
+            .map((entry, index) => {
+              let r = 0;
+              let g = 0;
+              let b = 0;
 
-      <ResponsiveContainer width="95%" height="90%">
-        {picked === 0 ? (
-          <></>
-        ) : (
-          <BarChart data={questions[picked - 1].replies}>
+              for(let i = 0; i < questions[picked].answers[index].length; i++) {
+                if (i % 3 === 0) {
+                  r += questions[picked].answers[index].charCodeAt(i);
+                } else if (i % 3 === 1) {
+                  g += questions[picked].answers[index].charCodeAt(i);
+                } else {
+                  b += questions[picked].answers[index].charCodeAt(i);
+                }
+              }
+
+              if (questions[picked].answers[index].length % 3 == 0) {
+                r = (r % 128);
+                g = 0;
+                b = (b % 128);  
+              } else if (questions[picked].answers[index].length % 3 == 1) {
+                r = 0;
+                g = (g % 128);
+                b = (b % 128);  
+              } else if (questions[picked].answers[index].length % 3 == 2) {
+                r = 0;
+                g = 0;
+                b = (b % 128);  
+              } else if (questions[picked].answers[index].length % 3 == 3) {
+                r = 0;
+                g = (g % 128);
+                b = 0;  
+              } else if (questions[picked].answers[index].length % 3 == 4) {
+                r = (r % 128);
+                g = 0;
+                b = 0;  
+              } else {
+                r = (r % 128);
+                g = (g % 128);
+                b = 0; 
+              }
+
+              return {
+                value: questions[picked].answers[index],
+                count: entry.answers,
+                color: `rgb(${r}, ${g}, ${b})`,
+              };
+            })
+            .filter((entry) => entry.count && entry.count > 0)}
+        />
+      ) : (
+        <ResponsiveContainer width="95%" height="90%">
+          <BarChart data={questions[picked].replies}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
             <Legend />
             <Bar dataKey="answers" fill="rgb(179, 144, 79)">
-              {questions[picked - 1].replies.map((entry, index) => (
+              {questions[picked].replies.map((entry, index) => (
                 <Cell
                   fill={
-                    questions[picked - 1].replies[index].isCorrect
+                    questions[picked].replies[index].isCorrect
                       ? "rgb(146, 208, 80)"
                       : "rgb(192, 80, 77)"
                   }
@@ -128,8 +181,8 @@ const Stats = ({state, dispatch}) => {
               ))}
             </Bar>
           </BarChart>
-        )}
-      </ResponsiveContainer>
+        </ResponsiveContainer>
+      )}
     </div>
   );
 };
